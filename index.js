ba97b5c27d74e1f001b05d25609e0c885cdd32c8
leejaeman2 <lividious2@naver.com>
2022-12-01 14:53:31 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config/key.js');
+
+const { User } = require('./models/User.js');
 
 app.set('port', process.env.PORT || 2031);
 const port = app.get('port');
 
-mongoose.connect(`mongodb+srv://leejaeman:1234qwer@boilerplate.i4hmian.mongodb.net/?retryWrites=true&w=majority`, {
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // application/json
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log(`MongoDB Connected...`))
   .catch(err => console.error(err));
 
-app.get('/', (req, res) => res.send(`<h1>우리집에 왜 왔니?</h1>`));
+app.get('/', (req, res) => res.send(`<h1>BODY 정보를 가져왔으면 좋겟다</h1>`));
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다v
+  console.log(`req.body: `);
+  console.dir(req.body);
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if(err) {
+      return res.json({ success: false, read: true, err });
+    }
+
+    return res.status(200).json({
+      success: true
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}`);
